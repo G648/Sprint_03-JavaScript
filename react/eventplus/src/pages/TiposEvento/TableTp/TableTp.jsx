bda08bb9c88a01df355c4d683cf543ab99c64a3d
Guilherme Amorim <cezarguilherme03@gmail.com>
2023-11-17 10:58:45 -0300

    fazendo funcionalidade de atualizar na tela de tipos de evento

--- react/eventplus/src/pages/TiposEvento/TableTp/TableTp.jsx
+++ react/eventplus/src/pages/TiposEvento/TableTp/TableTp.jsx
@@ -23,13 +23,20 @@ const TableTp = ({ dados, fnDelete = null, fnUpdate = null }) => {
 
                 {dados.map((tp) => {
                     return (
-                        <tr className="table-data__head-row">
+                        <tr className="table-data__head-row" key={tp.idTipoEvento}>
                             <td className="table-data__data table-data__data--big">
                                 {tp.titulo}
                             </td>
 
                             <td className="table-data__data table-data__data--little">
-                                <img className="table-data__icon" src={editPen} alt="" />
+                                <img
+                                    className="table-data__icon"
+                                    src={editPen}
+                                    alt=""
+                                    onClick={() => {
+                                        fnUpdate(tp.idTipoEvento)
+                                    }}
+                                />
                             </td>
 
                             <td
--- react/eventplus/src/pages/TiposEvento/TiposEvento.jsx
+++ react/eventplus/src/pages/TiposEvento/TiposEvento.jsx
@@ -9,6 +9,7 @@ import typeEventImage from '../../assets/icons/tipo-evento.svg'
 import ImageIlustrator from '../../Components/ImageIlustrator/ImageIlustrator';
 import { Button, Input } from '../../Components/FormComponents/FormComponents';
 import TableTp from './TableTp/TableTp';
+import Notification from '../../Components/Notification/Notification';
 
 import api, { getTitleEventsResources, postTitleEventsResources } from '../../Services/Service';
 
@@ -19,6 +20,7 @@ const TiposEvento = () => {
   const [frmEdit, setFrmEdit] = useState(false)
   const [titulo, setTitulo] = useState("")
   const [tiposEvento, setTiposEventos] = useState([])
+  const [notifyUser, setNotifyUser] = useState();
 
   //usado para chamar a API
   useEffect(() => {
@@ -36,7 +38,19 @@ const TiposEvento = () => {
     }
 
     loadEventsType();
-  }, [tiposEvento])
+  }, [])
+
+
+  function notificationMessage() {
+    setNotifyUser({
+      titleNote: "Sucesso",
+      textNote: `evento cadastrado com sucesso`,
+      imgIcon: "sucsess",
+      imgAlt:
+        "imagem de ilustração de sucesso. Moça segurando um balão com símbolo de confirmação OK",
+      showMessage: true
+    });
+  }
 
   //definindo a função async para nos conectarmos na API
   async function handleSubmit(e) {
@@ -62,8 +76,11 @@ const TiposEvento = () => {
   }
 
   // ************************EDITAR O CADASTRO DOS TIPOS EVENTOS********************************
-  function handleUpdate() {
-    alert('bora editar')
+  async function handleUpdate(e) {
+    e.preventDefault();
+    // alert(`vamos editar o evento de id ${idtipoevento}`)
+
+    
   }
 
   //apaga o tipo de evento na api 
@@ -77,11 +94,25 @@ const TiposEvento = () => {
 
     try {
 
-      const retorno = await api.delete(`${getTitleEventsResources}/${idtipoevento}`);  
-      
+      const retorno = await api.delete(`${getTitleEventsResources}/${idtipoevento}`);
+
       if (retorno.status == 204) {
 
-        alert("Apagado com sucesso!")
+        // setTiposEventos([tiposEvento]);
+
+        //fazendo nova requisição para atualizar nossa página depois que um item foi removido
+        const buscaEventos = await api.get(getTitleEventsResources)
+
+        setTiposEventos(buscaEventos.data);
+
+        setNotifyUser({
+          titleNote: "Sucesso",
+          textNote: `evento excluído com sucesso`,
+          imgIcon: "success",
+          imgAlt:
+            "imagem de ilustração de sucesso. Moça segurando um balão com símbolo de confirmação OK",
+          showMessage: true
+        });
 
       }
 
@@ -94,17 +125,38 @@ const TiposEvento = () => {
 
   //cancela a tela/ação de edição (volta para o form de cadastro)
   function editActionAbort() {
-    alert("cancelar a tela de edição dos dados")
+
+    setFrmEdit(false)
+
   }
 
   // mostra o formulário de edição
-  function showUpdateForm() {
-    alert(`vamos mostrar o formulário de edição`)
+  async function showUpdateForm(idtipoevento) {
+
+    setFrmEdit(true)
+
+    try {
+      const retorno = await api.put(`${getTitleEventsResources}/${idtipoevento}`)
+
+      setTitulo(retorno.data.titulo)
+
+    } catch (error) {
+      setNotifyUser({
+        titleNote: "warning",
+        textNote: `deu ruim na atualização`,
+        imgIcon: "warning",
+        imgAlt:
+          "imagem de ilustração de sucesso. Moça segurando um balão com símbolo de confirmação OK",
+        showMessage: true
+      });
+      console.log(error);
+    }
 
   }
 
   return (
     <>
+      {<Notification {...notifyUser} setNotifyUser={setNotifyUser} />}
 
       <Main>
         <section className='cadastro-evento-section'>
@@ -137,6 +189,7 @@ const TiposEvento = () => {
                         manipulationFunction={(e) => {
                           setTitulo(e.target.value)
                         }}
+
                       />
 
                       <Button
@@ -144,11 +197,51 @@ const TiposEvento = () => {
                         textButton="Cadastrar"
                         name="cadastrar"
                         type="submit"
+                        manipulationFunction={notificationMessage}
                       />
+                      {/* <Button
+                        id="mágica"
+                        textButton="mágica"
+                        name="magica"
+                        type="button"
+                        manipulationFunction={theMagic}
+                      /> */}
                     </>
                   )
                     : (
-                      <p>tela de atualizar</p>
+                      <>
+
+                        <Input
+                          id={"Atualizar"}
+                          placeholder={titulo}
+                          name={"atualiza"}
+                          type={"text"}
+                          required={"required"}
+                          value={titulo}
+                          manipulationFunction={(e) => {
+                            setTitulo(e.target.value)
+                          }}
+                        />
+                        <div className='buttons-editbox '>
+
+                          <Button
+                            id="Atualizar"
+                            textButton="Atualizar"
+                            name="Atualizar"
+                            type="submit"
+                          />
+
+                          <Button
+                            id="Cancela"
+                            textButton="Cancelar"
+                            name="Cancelar"
+                            type="button"
+                            manipulationFunction={editActionAbort}
+                          />
+                        </div>
+
+                      </>
+
                     )
                 }
 
